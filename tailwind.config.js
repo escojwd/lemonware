77281e77b9315f911aaf14745e7e0a098d37ddd1
escojwd <junior19941703@gmail.com>
2021-09-11 15:12:54 +0400

    feat: assets from figma

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,10 @@ module.exports = {
   purge: ['index.html'],
   darkMode: false, // or 'media' or 'class'
   theme: {
+      fontFamily: {
+        poppins: ['Poppins', 'sans-serif'],
+        inter: ['Inter', 'sans-serif'],
+      },
     extend: {},
   },
   variants: {
