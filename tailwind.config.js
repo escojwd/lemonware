ec066d7476555a540a94d6172aa826f4de243254
escojwd <junior19941703@gmail.com>
2021-09-11 20:06:59 +0400

    feat: config for spacing, width, etc.

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,16 +29,49 @@ module.exports = {
             'focused': '#B00000',
             'unfocused': '#D6D4D4',
             'header': '#2E2E2E',
-            'text-regular': '#808080',
+            'regular': '#808080',
             'title': '#18191F',
             'background': '#F7FAFE',
             'line-color': '#C4C4C4',
             'footer-text': '#E5E5E5'
+        },
+        spacing: {
+            '1px': '1px',
+            '3px': '3px',
+            '4px': '4px',
+            '5px': '5px',
+            '10px': '10px',
+            '12px': '12px',
+            '14px': '14px',
+            '18px': '18px',
+            '20px': '20px',
+            '23px': '23px',
+            '25px': '25px',
+            '46px': '46px',
+            '58px': '58px',
+            '60px': '60px',
+
+        },
+        width: {
+            '50px': '50px',
+            '90px': '90px',
+            '310px': '310px',
+            '500px': '500px',
+            '600px': '600px',
+            '86%': '86%',
+            'full-plus-16': 'calc(100% + 16px)'
+        },
+        opacity: {
+            '88': '0.88'
+        },
+        borderWidth: {
+            '1px': '1px'
+        },
+        lineHeight: {
+            '46px': '46px'
         }
     },
   },
-  variants: {
-    extend: {},
-  },
+  variants: {},
   plugins: [],
 }
